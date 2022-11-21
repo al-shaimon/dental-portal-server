5620f95d0a0ad7bb21c7b6a9d83d2bb8b96b7475
al-shaimon <alshaimon968@gmail.com>
2022-11-21 06:08:13 +0600

    77-9 Store payment data and loader spinner

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ async function run() {
     const bookingsCollection = client.db('doctorsPortal').collection('bookings');
     const usersCollection = client.db('doctorsPortal').collection('users');
     const doctorsCollection = client.db('doctorsPortal').collection('doctors');
+    const paymentsCollection = client.db('doctorsPortal').collection('payments');
 
     // NOTE: make sure you use verifyAdmin after verifyJWT
     const verifyAdmin = async (req, res, next) => {
@@ -195,7 +196,22 @@ async function run() {
       });
       res.send({
         clientSecret: paymentIntent.client_secret,
-      })
+      });
+    });
+
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const result = await paymentsCollection.insertOne(payment);
+      const id = payment.bookingId;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transectionId,
+        },
+      };
+      const updatedResult = await bookingsCollection.updateOne(filter, updatedDoc);
+      res.send(result);
     });
 
     app.get('/jwt', async (req, res) => {
